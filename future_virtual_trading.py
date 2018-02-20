6e9df4f0a6fb269d8c00ed04b428f3074a18b256
eric.feuilelaubois@sfr.fr <Rapido1->
2018-02-20 17:36:34 +0100

    t

--- future_virtual_trading.py
+++ future_virtual_trading.py
@@ -15,7 +15,7 @@ from pytz import timezone
 est = timezone('US/Eastern')
 
 
-from twilio.rest import Client tr
+from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
 account_sid = "AC3899557667afcb2bbc20e9ebecf8befb"
